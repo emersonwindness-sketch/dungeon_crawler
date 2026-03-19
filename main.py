589a5943908117619902a500ac4dddd35247ad3f
Emerson <emersonwindness@gmail.com>
2026-03-19 19:02:36 -0300

    C: world update

--- main.py
+++ main.py
@@ -1,2 +1,34 @@
 print ("Welcome to the depricated, dark and dead world after the awakening")
-print ("also commiting here just to thest vs_code")
+
+world = {
+    "Road":{
+        "description": "A comfortable place to stay in the night, you walked days to get here, hiding in the shadows, avoiding anything and everyone, after all, you don't really know who is what they claim to be, well, not after the falling.",
+        "choice": ["Forest", "Tavern"]
+    },
+    "Tavern":{
+        "description": " Description here for the Tavern ",
+        "choice": ["Room", "Road"]
+    },
+    "Forest":{
+        "description": " Description here for the Forest ",
+        "choice": ["Cave", "South", "Road"]
+    },
+       
+}
+
+def display_room(room):
+
+    room_data = world[room]
+    print (room_data)
+    
+current_room = "Road"    
+while True:
+
+    player_choice = input(f"what would you like to do?").capitalize()
+    print (f"you choose to: {player_choice}")
+    if player_choice in world:
+        current_room = player_choice
+        display_room(current_room)
+    else:
+        print ("Cannot go there!")
+    
