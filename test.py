bf6983adca59e32f903c629913e899a8dcc56a9e
Emerson <emersonwindness@gmail.com>
2026-03-22 10:25:18 -0300

    global move function fully complete, need to implement to inventory.py

--- test.py
+++ test.py
@@ -1,14 +1,13 @@
 Inventory = {
-        
-    "Bag":{
+    
         "Gold":{"Value": 50
         },
         "Dagger":{"Value": 1
         },
         "Hp potion": {"Value": 3
         },
+
         }
-    }
 
 Chest = {}
 
@@ -30,10 +29,41 @@ while loop is True:
                 Chest[Item_name]["Value"] = num_items
                 Inventory["Bag"][Item_name]["Value"] -= num_items
 
+    def transfer_items(source, destination, item_name, num_item):
 
-    player_choice = input("What item from inventory would you like to keep on your chest?\n").capitalize()
-    num_items = int(input("How many do you want to move?\n"))
-    move_item = move_inventory_to_chest(player_choice, num_items)
+        if item_name not in source:
+            print (f"The item '{item_name}' does not exist.")
+            return
+        if source[item_name]["Value"] <= 0:
+            print (f"You have no {item_name}")
+        else:
 
-    print (f"Current items in chest: {Chest}")
-    print (f"Current items in inventory: {Inventory}")
+            if item_name in destination:
+                destination[item_name]["Value"] += num_item
+                source[item_name]["Value"] -= num_item
+                return
+
+            if item_name not in destination:
+                destination[item_name] = {"Value": 0}
+                destination[item_name]["Value"] = num_item
+                source[item_name]["Value"] -= num_item
+                return
+
+
+    source = input("You want to move your inventory or take from chest?\nMove / Take: ").capitalize()
+    item_name = input("What item?").capitalize()
+    num_item = int(input("How many?"))
+
+    if source == "Move":
+        source = Inventory
+        destination = Chest
+        transfer_items(source, destination, item_name, num_item)
+
+    elif source == "Take":
+        source = Chest
+        destination = Inventory
+        transfer_items(source, destination, item_name, num_item)
+
+    print (f"Your inventory:\n {Inventory}")
+    print (f"Your chest:\n {Chest} ")
+
