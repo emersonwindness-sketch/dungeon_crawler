60df901e0a2e9cf36c57a5258f94e6e327fd9739
Emerson <emersonwindness@gmail.com>
2026-03-21 21:39:28 -0300

    fully functional chest

--- test.py
+++ test.py
@@ -12,22 +12,30 @@ Inventory = {
 
 Chest = {}
 
+loop = True
 
-def move_inventory_to_chest(Item_name, num_items):
-    
-    if Item_name in Chest:
-        Chest[Item_name] += Inventory["Bag"][Item_name]["Value"][num_items]
-        Chest[Item_name]["Value"] += num_items
+while loop is True:
+
+    def move_inventory_to_chest(Item_name, num_items):
+        
+        if Inventory["Bag"][Item_name]["Value"] <= 0:
+            print ("You have none of this item")
+        else: 
+            if Item_name in Chest:
+                Chest[Item_name] += Inventory["Bag"][Item_name]["Value"][num_items]
+                Chest[Item_name]["Value"] += num_items
+                Inventory[Item_name]["Value"] -= num_items
 
 
-    elif Item_name not in Chest:
-        Chest[Item_name] = {"Value": Inventory["Bag"][Item_name]["Value"] }
-        Chest[Item_name]["Value"] = num_items
+            elif Item_name not in Chest:
+                Chest[Item_name] = {"Value": Inventory["Bag"][Item_name]["Value"] }
+                Chest[Item_name]["Value"] = num_items
+                Inventory["Bag"][Item_name]["Value"] -= num_items
 
 
-player_choice = input("What item from inventory would you like to keep on your chest?\n").capitalize()
-num_items = int(input("How many do you want to move?\n"))
-move_item = move_inventory_to_chest(player_choice, num_items)
+    player_choice = input("What item from inventory would you like to keep on your chest?\n").capitalize()
+    num_items = int(input("How many do you want to move?\n"))
+    move_item = move_inventory_to_chest(player_choice, num_items)
 
-print (f"Current items in chest: {Chest}")
-print (f"Current items in inventory: {Inventory}")
+    print (f"Current items in chest: {Chest}")
+    print (f"Current items in inventory: {Inventory}")
